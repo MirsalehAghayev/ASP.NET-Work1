b655780cc5ce83f28b3c88b84b79d2b71e761cf8
Mirsaleh <mirsalehaa@code.edu.az>
2022-10-05 16:36:12 +0400

    Table

---
+++ ASP.NET1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Agayev";
+            ViewBag.Surname = "Mirsaleh";
+            ViewBag.Age = 22;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+" + 1234567890;
+            return View();
+        }
+    }
+}
